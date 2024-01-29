1f06ac267bfc2f61287917dda1aff61589df1a59
Lighuen Restanio <lighuen.restanio@gmail.com>
2024-01-29 16:23:50 -0300

    Landing first section started

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,19 @@
 <template>
-  <UContainer class="justify-center flex py-4">
-    <UCard class="size-1/3">
-      <template #header>
-        <UContainer class="flex flex-col items-center">
-          <h1 class="text-2xl">Buy coffe</h1>
-          <h3>One coffe = 1.5 ETH</h3>
-        </UContainer>
-      </template>
-
-      <div class="flex justify-center items-center pb-4">
-        <div>
-          <UButton
-            @click="removeOneCoffe()"
-            icon="i-heroicons-minus"
-            size="md"
-            :ui="{ rounded: 'rounded-full' }"
-            color="indigo"
-            variant="outline"
-          ></UButton>
-        </div>
-        <div class="flex flex-col items-center px-6">
-          <h2>Quantity</h2>
-          <h1 class="text-8xl">{{ quantity }}</h1>
-        </div>
-        <div>
-          <UButton
-            @click="addOneCoffe()"
-            icon="i-heroicons-plus"
-            size="md"
-            :ui="{ rounded: 'rounded-full' }"
-            color="indigo"
-            variant="outline"
-          ></UButton>
-        </div>
+  <div>
+    <div class="grid grid-cols-2 items-center m-20">
+      <div>
+        <h1 class="text-5xl">Not a cup,</h1>
+        <h1 class="text-5xl">just a coffee</h1>
+        <h3 class="text-gray-400">Easy, contacless, decentralized, green.</h3>
+        <UButton color="orange" variant="soft" label="Connect" />
       </div>
       <div>
-        <UFormGroup label="Dirección de PINTA - Polygon">
-          <UInput placeholder="Enter your addres" v-model="value" />
-        </UFormGroup>
-        <ULink inactive-class="text-primary"> I dont have a wallet </ULink>
+       
       </div>
-
-      <template #footer>
-        <div class="flex justify-between items-center">
-          <h1>Total:</h1>
-          <h1 class="text-2xl">{{ total }}</h1>
-        </div>
-      </template>
-    </UCard>
-  </UContainer>
+    </div>
+  </div>
 </template>
 
-<script setup>
-const quantity = ref(0);
-const value = ref("");
-const total = computed(() => {
-  return quantity.value * 100;
-});
-
-function addOneCoffe() {
-  quantity.value += 1;
-}
-function removeOneCoffe() {
-  if (quantity.value === 0) return;
-  quantity.value -= 1;
-}
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
