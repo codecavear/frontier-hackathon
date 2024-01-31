a242c8b1541601ef2bbf185260bec15176a79843
Lighuen Restanio <lighuen.restanio@gmail.com>
2024-01-31 19:08:32 -0300

    Layout base ready to be styled

--- components/CoffeeForm.vue
+++ components/CoffeeForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <UCard class="w-[400px] max-w-full">
+  <UCard class="w-[400px] max-w-full cardC">
     <template #header>
       <div class="flex flex-col items-center gap-2">
         <div class="flex gap-2">
@@ -114,6 +114,9 @@ onMounted(async () => {
 });
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+  .cardC {
+    background: linear-gradient(to top, rgb(18, 18, 18) 10.6%, rgb(0, 0, 0) 97.7%); 
+    opacity: 0.1; }
 </style>
+#121212
--- pages/index.vue
+++ pages/index.vue
@@ -1,104 +1,107 @@
 <template>
-  <UContainer>
-    <div class="grid grid-cols-2 items-center text-center h-[70vh]">
-      <div class="flex flex-col gap-2 justify-start">
-        <h1 class="text-5xl">CFT</h1>
-        <h2 class="text-3xl">1 coffee......1 USDT</h2>
-        <h3 class="text-gray-400">
-          The only wallet in the market with a focus on NFTs.
-        </h3>
-      </div>
-
-      <div class="flex justify-center back-red">
-        <CoffeeForm />
-      </div>
-    </div>
-  </UContainer>
-
-  <UContainer>
-    <div class="grid grid-cols-3 items-center text-center h-[50vh]">
-      <div><UButton icon="i-heroicons-inbox" color="gray" size="xl" /></div>
-      <div><UButton icon="i-heroicons-inbox" color="gray" size="xl" /></div>
-      <div><UButton icon="i-heroicons-inbox" color="gray" size="xl" /></div>
-    </div>
-  </UContainer>
-
-  <UContainer>
-    <div class="grid grid-cols-2 items-center text-center h-[70vh]">
-      <div class="flex gap-2 flex-col">
-        <h3 class="text-orange-700">About the app</h3>
-        <h1 class="text-2xl">About the app</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-          dignissimos doloremque atque? Error omnis eius minus tempore nam natus
-          hic reprehenderit, corrupti quasi. Recusandae commodi iure, hic illo
-          error nam.
-        </p>
-        <div>
-          <UButton variant="ghost" color="purple"> Learn more </UButton>
+  <div>
+    <UContainer>
+      <div class="grid grid-cols-2 items-center text-center h-[70vh]">
+        <div class="flex flex-col gap-2 justify-start">
+          <h1 class="text-5xl">CFT</h1>
+          <h2 class="text-3xl">1 coffee......1 USDT</h2>
+          <h3 class="text-gray-400">
+            The only wallet in the market with a focus on NFTs.
+          </h3>
+        </div>
+        <div class="flex justify-center items-center h-[70vh]">
+          <CoffeeForm />
         </div>
       </div>
+    </UContainer>
 
-      <div>
-        <h1 class="text-3xl">aca va una img</h1>
+    <UContainer>
+      <div class="grid grid-cols-3 items-center text-center h-[50vh]">
+        <div><UButton icon="i-heroicons-inbox" color="gray" size="xl" /></div>
+        <div><UButton icon="i-heroicons-inbox" color="gray" size="xl" /></div>
+        <div><UButton icon="i-heroicons-inbox" color="gray" size="xl" /></div>
       </div>
-    </div>
-  </UContainer>
+    </UContainer>
 
-  <UContainer>
-    <div class="grid grid-cols-2 items-center text-center h-[70vh]">
-      <div>
-        <h1 class="text-3xl">aca va una img</h1>
-      </div>
-      <div>
-        <h3 class="text-orange-700">About the app</h3>
-        <h1 class="text-2xl">About the app</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-          dignissimos doloremque atque? Error omnis eius minus tempore nam natus
-          hic reprehenderit, corrupti quasi. Recusandae commodi iure, hic illo
-          error nam.
-        </p>
-        <div>
-          <UButton variant="ghost" color="purple"> Learn more </UButton>
+    <UContainer>
+      <div class="grid grid-cols-2 items-center text-center h-[70vh]">
+        <div class="flex gap-2 flex-col">
+          <h3 class="text-orange-700">About the app</h3>
+          <h1 class="text-2xl">About the app</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+            dignissimos doloremque atque? Error omnis eius minus tempore nam
+            natus hic reprehenderit, corrupti quasi. Recusandae commodi iure,
+            hic illo error nam.
+          </p>
+          <div>
+            <UButton variant="ghost" color="purple"> Learn more </UButton>
+          </div>
+        </div>
+
+        <div class="grid grid-rows-3 grid-flow-col gap-4">
+          <div class="row-start-5 row-span-2 ...">01</div>
+          <div class="row-end-3 row-span-2 ...">02</div>
+          
         </div>
       </div>
-    </div>
-  </UContainer>
+    </UContainer>
 
-  <UContainer>
-    <div class="grid grid-cols-2 items-center text-center h-[70vh]">
-      <div>
-        <h3 class="text-orange-700">About the app</h3>
-        <h1 class="text-2xl">About the app</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-          dignissimos doloremque atque? Error omnis eius minus tempore nam natus
-          hic reprehenderit, corrupti quasi. Recusandae commodi iure, hic illo
-          error nam.
-        </p>
+    <UContainer>
+      <div class="grid grid-cols-2 items-center text-center h-[70vh]">
         <div>
-          <UButton variant="ghost" color="purple"> Learn more </UButton>
+          <h1 class="text-3xl">aca va una img</h1>
+        </div>
+        <div>
+          <h3 class="text-orange-700">About the app</h3>
+          <h1 class="text-2xl">About the app</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+            dignissimos doloremque atque? Error omnis eius minus tempore nam
+            natus hic reprehenderit, corrupti quasi. Recusandae commodi iure,
+            hic illo error nam.
+          </p>
+          <div>
+            <UButton variant="ghost" color="purple"> Learn more </UButton>
+          </div>
         </div>
       </div>
+    </UContainer>
 
-      <div>
-        <h1 class="text-3xl">aca va una img</h1>
+    <UContainer>
+      <div class="grid grid-cols-2 items-center text-center h-[70vh]">
+        <div>
+          <h3 class="text-orange-700">About the app</h3>
+          <h1 class="text-2xl">About the app</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+            dignissimos doloremque atque? Error omnis eius minus tempore nam
+            natus hic reprehenderit, corrupti quasi. Recusandae commodi iure,
+            hic illo error nam.
+          </p>
+          <div>
+            <UButton variant="ghost" color="purple"> Learn more </UButton>
+          </div>
+        </div>
+
+        <div>
+          <h1 class="text-3xl">aca va una img</h1>
+        </div>
       </div>
-    </div>
-  </UContainer>
-  <UContainer>
-    <div class="grid grid-rows-3 grid-flow-col items-center text-center gap-4 h-[70vh]]">
-      <div class="row-span-4 col-span-2 ...">01</div>
-      <div class="row-span-2 col-span-2 ...">02</div>
-      <div class="row-span-2 ...">03</div>
-      <div class="row-span-2 ...">03</div>
-    </div>
-  </UContainer>
+    </UContainer>
+    <UContainer>
+      <div
+        class="grid grid-rows-3 grid-flow-col items-center text-center gap-4 h-[70vh]]"
+      >
+        <div class="row-span-4 col-span-2 ...">01</div>
+        <div class="row-span-2 col-span-2 ...">02</div>
+        <div class="row-span-2 ...">03</div>
+        <div class="row-span-2 ...">03</div>
+      </div>
+    </UContainer>
+  </div>
 </template>
 
 <script setup></script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
