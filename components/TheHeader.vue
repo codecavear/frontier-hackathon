dae8efa31ca42410d169e9bb5fc12f21c34f5b7a
nsalto <nahuelsalto.71@gmail.com>
2024-02-18 15:02:01 -0300

    impruve get user balance

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,41 +1,22 @@
 <template>
   <header class="fixed w-full">
-    <div
-      class="inset-x-0 bottom-0 container mx-auto flex justify-between items-center px-2 py-2 bg-transparent"
-    >
+    <div class="inset-x-0 bottom-0 container mx-auto flex justify-between items-center px-2 py-2 bg-transparent">
       <h1 class="text-white text-2xl font-bold">COFF</h1>
       <UKbd class="flex gap-2 items-center py-1 px-2" size="lg">
-        {{ nftBalance || 0 }} <div class="flex items-center">COFF<UIcon dynamic name="i-bx-coffee-togo"
-      /></div></UKbd>
+        {{ nftBalance || 0 }} <div class="flex items-center">COFF
+          <UIcon dynamic name="i-bx-coffee-togo" />
+        </div>
+      </UKbd>
       <ConnectWallet />
     </div>
   </header>
 </template>
 <script setup>
-import { readContract, getAccount } from "@wagmi/core";
-import coffeeABI from "@/abis/coffee.json";
-
-const account = getAccount();
-const nftBalance = ref("");
-const coffeContractAddress = "0x65Fe8c75Ed4B2e50D4E5E4CEdB2914a5ee7a0846";
-
-async function getNftBalance() {
-  nftBalance.value = await readContract({
-    abi: coffeeABI,
-    address: coffeContractAddress,
-    functionName: "balanceOf",
-    args: [account.address],
-  });
-}
-
-onMounted(async () => {
-  getNftBalance();
-});
+const { nftBalance } = useNftDetails();
 </script>
 <style scoped>
- /* .bg-translucid 
+/* .bg-translucid 
   {
   background-color: red;    
   } */
-
 </style>
--- composables/useNft.ts
+++ composables/useNft.ts
@@ -9,28 +9,43 @@ import coffeeABI from "../abis/coffee.json";
 import { onMounted, ref } from "vue";
 import { Hex } from "viem";
 
-const coffeContractAddress = "0x65Fe8c75Ed4B2e50D4E5E4CEdB2914a5ee7a0846";
+const coffeContractAddress = (process.env.COFFEE_CONTRACT_ADDRESS ||
+  "0x65Fe8c75Ed4B2e50D4E5E4CEdB2914a5ee7a0846") as Hex;
 
 export const useNftDetails = () => {
-
   const collateralSymbol = ref();
   const erc20TokenAddress = ref();
   const nftPrice = ref();
   const nftSymbol = ref();
   const mintingNft = ref(false);
+  const nftBalance = ref();
 
-  const { address } = getAccount();
+  const { address: userAddress } = getAccount();
+
+  async function getNftBalance() {
+    nftBalance.value = await readContract({
+      abi: coffeeABI,
+      address: coffeContractAddress,
+      functionName: "balanceOf",
+      args: [userAddress],
+    });
+  }
 
   async function mintNFT(quantity: number) {
+    if (!userAddress || !userAddress) {
+      console.error("No user address provided");
+      return;
+    }
+
     mintingNft.value = true;
     try {
       const currentAllowance = await readContract({
         abi: erc20ABI,
         address: erc20TokenAddress.value as Hex,
         functionName: "allowance",
-        args: [address, coffeContractAddress],
+        args: [userAddress, coffeContractAddress],
       });
-  
+
       if (BigInt(currentAllowance) < BigInt(quantity)) {
         // approve
         const approvalTransaction = await writeContract({
@@ -39,10 +54,10 @@ export const useNftDetails = () => {
           functionName: "approve",
           args: [coffeContractAddress, BigInt(quantity)],
         });
-  
+
         await waitForTransaction(approvalTransaction);
       }
-  
+
       // mint
       await writeContract({
         abi: coffeeABI,
@@ -50,15 +65,14 @@ export const useNftDetails = () => {
         functionName: "mintToken",
         args: [quantity.toString()],
       });
-  
     } finally {
       mintingNft.value = false;
-    }
 
-    
+      await getNftBalance();
+    }
   }
 
-  async function redeemNFT(tokenIds) {
+  async function redeemNFT(tokenIds: string[]) {
     try {
       const redeemTransaction = await writeContract({
         abi: coffeeABI,
@@ -70,6 +84,8 @@ export const useNftDetails = () => {
       console.log("Redeem ok");
     } catch (error) {
       console.error("Error redeeming nsfts", error);
+    } finally {
+      await getNftBalance();
     }
   }
 
@@ -85,6 +101,8 @@ export const useNftDetails = () => {
       console.log("Swap ok");
     } catch (error) {
       console.error("Error swapping nsfts", error);
+    } finally {
+      await getNftBalance();
     }
   }
 
@@ -113,8 +131,10 @@ export const useNftDetails = () => {
     collateralSymbol,
     nftPrice,
     nftSymbol,
+    nftBalance,
     mintNFT,
     mintingNft,
     transferNFT,
+    redeemNFT,
   };
 };
