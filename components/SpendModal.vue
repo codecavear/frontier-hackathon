b85d6ad51625b283c939e74d7fa7009fa2f0560c
Lighuen Restanio <lighuen.restanio@gmail.com>
2024-02-19 16:23:16 -0300

    x button added to spendmodal

--- components/SpendModal.vue
+++ components/SpendModal.vue
@@ -1,11 +1,22 @@
 <template>
-  <UModal v-model="props.isOpen" >
-    <UCard :ui="{
-      ring: '',
-      divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-    }">
+  <UModal v-model="props.isOpen">
+    <UCard
+      :ui="{
+        ring: '',
+        divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+      }"
+    >
       <template #header>
-        <h1 class="text-2xl">Spend your COFF</h1>
+        <div class="flex justify-between">
+          <h1 class="text-2xl">Spend your COFF</h1>
+          <UButton
+            color="gray"
+            variant="ghost"
+            icon="i-heroicons-x-mark-20-solid"
+            class="-my-1"
+            @click="isOpen = false"
+          />
+        </div>
       </template>
 
       <div class="gap-4 flex flex-col">
@@ -13,26 +24,36 @@
         <qrcode-stream @detect="onDetect"></qrcode-stream>
         <div>
           <label>Store address</label>
-          <UInput :autofocus="false" label="Store address" v-model="toAddress"></UInput>
+          <UInput
+            :autofocus="false"
+            label="Store address"
+            v-model="toAddress"
+          ></UInput>
         </div>
       </div>
       <template #footer>
-        <UButton block label="Spend" variant="soft" size="lg" @click="transferNFT(quantity, toAddress)" />
+        <UButton
+          block
+          label="Spend"
+          variant="soft"
+          size="lg"
+          @click="transferNFT(quantity, toAddress)"
+        />
       </template>
     </UCard>
   </UModal>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { QrcodeStream } from 'vue-qrcode-reader'
+import { ref } from "vue";
+import { QrcodeStream } from "vue-qrcode-reader";
 
 const { transferNFT } = useNftDetails();
 
 const props = defineProps(["isOpen"]);
 const emit = defineEmits(["update:isOpen"]);
 
-const toAddress = ref("")
+const toAddress = ref("");
 const quantity = ref(1);
 
 const onDetect = (detectedCodes: any) => {
